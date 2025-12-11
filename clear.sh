ddc8d1eed0b11cd588a1a1fab510618fb0abcedc
kali <sharktallica@gmail.com>
2025-12-11 18:07:44 +0000

    Added clear.sh helper for clearing environment

---
+++ clear.sh
@@ -0,0 +1,7 @@
+sudo rm -r build/ || true
+sudo rm -r busybox-*/ || true
+sudo rm -r linux/ || true
+sudo rm -r i486-linux-musl-cross/ || true
+sudo rm -r filesystem/ || true
+rm *.tar.gz || true
+rm *.tgz || true
