23cc6b0c3ba4e7f8106d1895fca0bc5ed2c769a9
kali <sharktallica@gmail.com>
2026-08-07 00:29:05 +0100

    Bump kernel version 7.1.6 to 7.1.7

--- shork-486/config.sh
+++ shork-486/config.sh
@@ -44,7 +44,7 @@ IS_ARCH=false
 IS_FEDORA=false
 IS_DEBIAN=true
 BUILD_TYPE="default"
-LINUX_VER="7.1.6"
+LINUX_VER="7.1.7"
 TARGET_DISK=100
 TARGET_SWAP=8
 SCANCODE_SET=-1
@@ -543,7 +543,7 @@ LINUX_VER=$(dialog --clear \
     --default-item "$LINUX_VER" \
     --menu "Please select which Linux kernel version you wish to use. It is generally safe to use the newest major version that isn't \"-rc\", but if you experience hardware compatibility issues, try building with an older kernel to see if that resolves them. If so, please report it as an issue on the SHORK 486 GitHub repository. Only select a \"-rc\" kernel if you know what you're doing." 14 $WIDTH 5 \
     "7.2-rc6"   "7.2-rc6 (2026-08-02, testing)" \
-    "7.1.6"     "7.1.6 (2026-08-03, stable)" \
+    "7.1.7"     "7.1.7 (2026-08-06, stable)" \
     "7.0.14"    "7.0.14 (2026-06-27, EOL)" \
     3>&1 1>&2 2>&3)
 
