b47b02944387b3acb9b42385204d5189685204fb
kali <22660310+SharktasticA@users.noreply.github.com>
2026-09-07 10:17:54 +0100

    Bump kernel versions 7.1.12 to 7.1.13, 7.2.2 to 7.2.3 and 7.3-rc1 to 7.3-rc2

--- shork-486/config.sh
+++ shork-486/config.sh
@@ -46,7 +46,7 @@ IS_ARCH=false
 IS_FEDORA=false
 IS_DEBIAN=true
 BUILD_TYPE="default"
-LINUX_VER="7.2.2"
+LINUX_VER="7.2.3"
 TARGET_DISK=100
 TARGET_SWAP=8
 SCANCODE_SET=-1
@@ -488,9 +488,9 @@ CHOICE=$(dialog --clear \
     --cancel-label "Quit" \
     --default-item "$CHOICE_DEFAULT" \
     --menu "Select the host environment you plan to build SHORK 486 with." 10 $WIDTH 3 \
-    "Arch"    "Native building on Arch" \
-    "Debian"  "Native building on Debian/Dockerised building" \
-    "Fedora"  "Native building on Fedora" \
+    "Arch"    "Native building on Arch-based distros" \
+    "Debian"  "Native building on Debian-based distros/Dockerised building" \
+    "Fedora"  "Native building on Fedora-based distros" \
     3>&1 1>&2 2>&3)
 
 if [[ ! -n "$CHOICE" ]]; then
@@ -565,9 +565,9 @@ LINUX_VER=$(dialog --clear \
     --cancel-label "Quit" \
     --default-item "$LINUX_VER" \
     --menu "Please select which Linux kernel version you wish to use. It is generally safe to use the newest major version that isn't \"-rc\", but if you experience hardware compatibility issues, try building with an older kernel to see if that resolves them. If so, please report it as an issue on the SHORK 486 GitHub repository. Only select a \"-rc\" kernel if you know what you're doing." 15 $WIDTH 5 \
-    "7.3-rc1"   "7.3-rc1 (2026-08-30, testing)" \
-    "7.2.2"     "7.2.2 (2026-08-28, stable)" \
-    "7.1.12"    "7.1.12 (2026-08-28, stable)" \
+    "7.3-rc2"   "7.3-rc2 (2026-09-06, testing)" \
+    "7.2.3"     "7.2.3 (2026-09-02, stable)" \
+    "7.1.13"    "7.1.13 (2026-09-02, EOL)" \
     "7.0.14"    "7.0.14 (2026-06-27, EOL)" \
     3>&1 1>&2 2>&3)
 
